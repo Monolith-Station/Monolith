6e56ada3b2fc7f750428629de7578cf652656102
Errant <35878406+Errant-4@users.noreply.github.com>
2025-03-25 17:03:59 +0100

    Admin playerlist antag presentation rework (#35538)
    * refactor(src): Minor refactor of Draw in "AdminNameOverlay. And new info about playtime player
    
    * fix(src): Add configure classic admin owerlay
    
    * fix
    
    * antag status indication rework
    
    * the cvars are free, you can just take them
    
    * update playerlist on cvar change
    
    * more overlay options
    
    * tweak(src): Use _antagLabelClassic and tweak style
    
    * tweak(src): Add config display overlay for startingJob and playTime
    
    * tweak(src): Vector2 is replaced by var
    
    * tweak(src): return to the end of the list
    
    * add new option checkboxes
    
    * passing ConfigurationManager through constructor, some format changes
    
    * made sorting values more futureproof
    
    * comments
    
    * labels
    
    * no point commenting this out when the overlay stack PR will uncomment it again anyway
    
    * sorting prototype
    
    * localize symbols because why not
    
    * symmetry
    
    * Revert "localize symbols because why not"
    
    This reverts commit 922d4030300285a45777d62fcfd9c74b25fe7a60.
    
    * layout and formatting stuff
    
    * fix errant space
    
    ---------
    
    Co-authored-by: Schrödinger <132720404+Schrodinger71@users.noreply.github.com>

--- Content.Server/Administration/Systems/AdminSystem.cs
+++ Content.Server/Administration/Systems/AdminSystem.cs
@@ -233,6 +233,7 @@ public sealed class AdminSystem : EntitySystem
         var entityName = string.Empty;
         var identityName = string.Empty;
         int balance = int.MinValue; // Frontier
+        var sortWeight = 0;
 
         if (session?.AttachedEntity != null)
         {
@@ -249,8 +250,10 @@ public sealed class AdminSystem : EntitySystem
 
         RoleTypePrototype roleType = new();
         var startingRole = string.Empty;
-        if (_minds.TryGetMind(session, out var mindId, out var mindComp))
+        if (_minds.TryGetMind(session, out var mindId, out var mindComp) && mindComp is not null)
         {
+            sortWeight = _role.GetRoleCompByTime(mindComp)?.Comp.SortWeight ?? 0;
+
             if (_proto.TryIndex(mindComp.RoleType, out var role))
                 roleType = role;
             else
@@ -269,8 +272,20 @@ public sealed class AdminSystem : EntitySystem
             overallPlaytime = playTime;
         }
 
-        return new PlayerInfo(name, entityName, identityName, startingRole, antag, roleType, GetNetEntity(session?.AttachedEntity), data.UserId,
-            connected, _roundActivePlayers.Contains(data.UserId), overallPlaytime, balance); // Frontier: added balance
+        return new PlayerInfo(
+            name,
+            entityName,
+            identityName,
+            startingRole,
+            antag,
+            roleType,
+            sortWeight,
+            GetNetEntity(session?.AttachedEntity),
+            data.UserId,
+            connected,
+            _roundActivePlayers.Contains(data.UserId),
+            overallPlaytime,
+            balance); // Frontier
     }
 
     private void OnPanicBunkerChanged(bool enabled)
