3fcdebbf2bed1a69d19880822088243851d69a37
NazrinNya <macrohard@macrohard.localhost>
2026-01-03 23:37:29 +0300

    update

---
+++ Content.Server/_Mono/DeviceLinking/RotationSignalControlComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server._Mono.DeviceLinking;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class RotationSignalControlComponent : Component
+{
+    
+}
---
+++ Content.Server/_Mono/DeviceLinking/RotationSignalControlSystem.cs
@@ -0,0 +1,13 @@
+namespace Content.Server._Mono.DeviceLinking;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class RotationSignalControlSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        
+    }
+}
