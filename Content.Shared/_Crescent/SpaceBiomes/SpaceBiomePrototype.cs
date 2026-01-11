7d8c64d40d3ff1836070b209ed9bf5ef6c5aa1d1
point2 <doublea654@gmail.com>
2026-01-11 10:59:29 +0100

    bye bye parallax tag

--- Content.Shared/_Crescent/SpaceBiomes/SpaceBiomePrototype.cs
+++ Content.Shared/_Crescent/SpaceBiomes/SpaceBiomePrototype.cs
@@ -8,9 +8,6 @@ public sealed class SpaceBiomePrototype : IPrototype
     [IdDataField]
     public string ID { get; private set; } = default!;
 
-    [DataField(required: true)] //can't be automatically validated since parallax prototype is available only for clients
-    public string Parallax = "";
-
     [DataField(required: true)]
     public string Name = "";
 
