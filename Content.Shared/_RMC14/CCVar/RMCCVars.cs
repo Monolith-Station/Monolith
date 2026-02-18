240dc4f2819d002f86f8aaf2dafb977271db3f30
harryhulse12-alt <harryhulse12@gmail.com>
2026-02-18 09:46:56 +0000

    Deadchat (#554)
    * removes guerilla gloves from uplink
    
    * oops
    
    * Add Disable Deadchat Command called `setdchat` (#7240)
    
    Co-authored-by: DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
    
    * REmoves irrelevant CVAR
    
    * Using
    
    * fix?
    
    ---------
    
    Co-authored-by: PursuitInAshes <pursuitinashes@gmail.com>
    Co-authored-by: DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>

--- Content.Shared/_RMC14/CCVar/RMCCVars.cs
+++ Content.Shared/_RMC14/CCVar/RMCCVars.cs
@@ -4,8 +4,11 @@ using Robust.Shared.Configuration;
 namespace Content.Shared._RMC14.CCVar;
 
 [CVarDefs]
-public sealed class RMCCVars : CVars
+public sealed partial class RMCCVars : CVars
 {
+    public static readonly CVarDef<bool> RMCDeadChatEnabled =
+        CVarDef.Create("rmc.dead_chat_enabled", true, CVar.SERVER | CVar.NOTIFY | CVar.REPLICATED);
+
     public static readonly CVarDef<bool> RMCGunPrediction =
         CVarDef.Create("rmc.gun_prediction", true, CVar.SERVER | CVar.REPLICATED);
 
