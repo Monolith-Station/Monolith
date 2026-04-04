ed4e303bf3f2ea8d42069b5e870665023c513810
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2026-04-04 02:32:57 -0400

    death

--- Content.Server/_Mono/Traits/Physical/ThresholdOffsetSystem.cs
+++ Content.Server/_Mono/Traits/Physical/ThresholdOffsetSystem.cs
@@ -6,15 +6,15 @@ namespace Content.Server._Mono.Traits.Physical;
 /// <summary>
 /// Applies the Will To Live trait effects by modifying the death health threshold.
 /// </summary>
-public sealed class ThresholdOffsetSystem : EntitySystem
+public sealed class MobThresholdOffsetSystem : EntitySystem
 {
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<ThresholdOffsetComponent, QueryMobThresholdsEvent>(OnQueryMobThresholds);
+        SubscribeLocalEvent<MobThresholdOffsetComponent, QueryMobThresholdsEvent>(OnQueryMobThresholds);
     }
 
-    private void OnQueryMobThresholds(EntityUid uid, ThresholdOffsetComponent component, QueryMobThresholdsEvent ev)
+    private void OnQueryMobThresholds(EntityUid uid, MobThresholdOffsetComponent component, QueryMobThresholdsEvent ev)
     {
         ev.DeathOffset += component.DeadOffset;
         ev.CritOffset += component.CritOffset;
