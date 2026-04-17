suziford/Suzi-Station

Mapping editor - early merge 'n tweaks (#82)
* add layers menu and erase tile button

* add fixgridatmos and rmgrid buttons

* remove delete button

* fix texture

* add icons

* move layers buttons to xaml

* add move grid button

* add secondary use cancel actions

* add grid VV button and decales parents

* add shaddow toggle button

* idk what i do

* add erase decal overlay

* rename layers window to visibility window

* rework editor ui layout

* cleanup

* better pick action overlay

* add gallery layout for decals

* unfuck fucking ui

* better icons

* fix gallery layout search

* locale and rename stuff

* add dical picker

* fix

* decal color pick

* add decal switch on middle mouse

* better bindings

* fixes and cleanup

* add use custom color button

* fixes

* add favorite objects list

* add search for entities by prototype id

* rename

* add mapping objects templates

* add pipes color action and decal rotation

* fix escape menu in editor

* add chat toggle button

* save favorites objects locally

* todo

* update mappingclientsidesetup command

* fix admin menu in editor

* update mapping templates

* add buttons tooltip

* add palette for atmos pipes

* comments and code cleanup

* remove mapping overlay disabling on server

* cleanup

* fix decal pick bind

* fix yaml linter

* split pr into parts

* fix null prototypes search

* review

* forgot

* fix typecheck

* hide HideSpawnMenu entities

* add show/hide spawn lists button

* open object tree on double click in search list

* fix human-readable names for tiles

* fix objects dublicating

* webedit ops

* Update MappingOverlay.cs

* chore: Automatically update REUSE headers

* chore: Automatically update REUSE headers

* get everything working

* chore: Automatically update REUSE headers

* chore: Automatically update REUSE headers

* whitespace

* tags for mapping

* tag fix

* markings, comments and namespace

* chore: Automatically update REUSE headers

* mapping templates update part

Content.Client/Mapping/MappingManager.cs
@@ -70,15 +70,24 @@
 // SPDX-FileCopyrightText: 2024 to4no_fix <156101927+chavonadelal@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 voidnull000 <18663194+voidnull000@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
+// SPDX-FileCopyrightText: 2025 poemota <142114334+poeMota@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Content.Shared.Decals;
 using Content.Shared.Mapping;
+using Content.Shared.Maps;
 using Robust.Client.UserInterface;
 using Robust.Shared.Network;
+using Robust.Shared.Prototypes;
 
 namespace Content.Client.Mapping;
 
@@ -86,15 +95,21 @@ public sealed class MappingManager : IPostInjectInit
 {
     [Dependency] private readonly IFileDialogManager _file = default!;
     [Dependency] private readonly IClientNetManager _net = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!; //Reserve - Wizden mapping editor
 
     private Stream? _saveStream;
     private MappingMapDataMessage? _mapData;
+    private List<IPrototype>? _favoritePrototypes;
+
+    public event Action<List<IPrototype>>? OnFavoritePrototypesLoaded;
 
     public void PostInject()
     {
         _net.RegisterNetMessage<MappingSaveMapMessage>();
         _net.RegisterNetMessage<MappingSaveMapErrorMessage>(OnSaveError);
         _net.RegisterNetMessage<MappingMapDataMessage>(OnMapData);
+        _net.RegisterNetMessage<MappingFavoritesDataMessage>(OnFavoritesData); //Reserve - Wizden mapping editor
+        _net.RegisterNetMessage<MappingFavoritesSaveMessage>(); //Reserve - Wizden mapping editor
     }
 
     private void OnSaveError(MappingSaveMapErrorMessage message)
@@ -117,6 +132,25 @@ public sealed class MappingManager : IPostInjectInit
         _saveStream = null;
         _mapData = null;
     }
+
+    //Reserve - Wizden mapping editor begin
+    private void OnFavoritesData(MappingFavoritesDataMessage message)
+    {
+        _favoritePrototypes = new List<IPrototype>();
+
+        foreach (var prototype in message.PrototypeIDs)
+        {
+            if (_prototypeManager.TryIndex<EntityPrototype>(prototype, out var entity))
+                _favoritePrototypes.Add(entity);
+            else if (_prototypeManager.TryIndex<ContentTileDefinition>(prototype, out var tile))
+                _favoritePrototypes.Add(tile);
+            else if (_prototypeManager.TryIndex<DecalPrototype>(prototype, out var decal))
+                _favoritePrototypes.Add(decal);
+        }
+
+        OnFavoritePrototypesLoaded?.Invoke(_favoritePrototypes);
+    }
+    //Reserve - Wizden mapping editor end
 
     public async Task SaveMap()
     {
@@ -141,4 +175,23 @@ public sealed class MappingManager : IPostInjectInit
 
         _saveStream = stream;
     }
+    //Reserve - Wizden mapping editor begin
+    public void SaveFavorites(List<MappingPrototype> prototypes)
+    {
+        var msg = new MappingFavoritesSaveMessage()
+        {
+            PrototypeIDs = prototypes
+                .FindAll(p => p.Prototype != null)
+                .Select(p => p.Prototype!.ID)
+                .ToList(),
+        };
+        _net.ClientSendMessage(msg);
+    }
+
+    public void LoadFavorites()
+    {
+        var request = new MappingFavoritesLoadMessage();
+        _net.ClientSendMessage(request);
+    }
+    //Reserve - Wizden mapping editor end
 }

Monolith-Station/Monolith

Content.Client/Mapping/MappingManager.cs
@@ -1,9 +1,12 @@
-﻿using System.IO;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Content.Shared.Decals;
 using Content.Shared.Mapping;
+using Content.Shared.Maps;
 using Robust.Client.UserInterface;
 using Robust.Shared.Network;
+using Robust.Shared.Prototypes;
 
 namespace Content.Client.Mapping;
 
@@ -11,15 +14,21 @@ public sealed class MappingManager : IPostInjectInit
 {
     [Dependency] private readonly IFileDialogManager _file = default!;
     [Dependency] private readonly IClientNetManager _net = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!; //Reserve - Wizden mapping editor
 
     private Stream? _saveStream;
     private MappingMapDataMessage? _mapData;
+    private List<IPrototype>? _favoritePrototypes;
+
+    public event Action<List<IPrototype>>? OnFavoritePrototypesLoaded;
 
     public void PostInject()
     {
         _net.RegisterNetMessage<MappingSaveMapMessage>();
         _net.RegisterNetMessage<MappingSaveMapErrorMessage>(OnSaveError);
         _net.RegisterNetMessage<MappingMapDataMessage>(OnMapData);
+        _net.RegisterNetMessage<MappingFavoritesDataMessage>(OnFavoritesData); //Reserve - Wizden mapping editor
+        _net.RegisterNetMessage<MappingFavoritesSaveMessage>(); //Reserve - Wizden mapping editor
     }
 
     private void OnSaveError(MappingSaveMapErrorMessage message)
@@ -42,6 +51,25 @@ public sealed class MappingManager : IPostInjectInit
         _saveStream = null;
         _mapData = null;
     }
+
+    //Reserve - Wizden mapping editor begin
+    private void OnFavoritesData(MappingFavoritesDataMessage message)
+    {
+        _favoritePrototypes = new List<IPrototype>();
+
+        foreach (var prototype in message.PrototypeIDs)
+        {
+            if (_prototypeManager.TryIndex<EntityPrototype>(prototype, out var entity))
+                _favoritePrototypes.Add(entity);
+            else if (_prototypeManager.TryIndex<ContentTileDefinition>(prototype, out var tile))
+                _favoritePrototypes.Add(tile);
+            else if (_prototypeManager.TryIndex<DecalPrototype>(prototype, out var decal))
+                _favoritePrototypes.Add(decal);
+        }
+
+        OnFavoritePrototypesLoaded?.Invoke(_favoritePrototypes);
+    }
+    //Reserve - Wizden mapping editor end
 
     public async Task SaveMap()
     {
@@ -66,4 +94,23 @@ public sealed class MappingManager : IPostInjectInit
 
         _saveStream = stream;
     }
+    //Reserve - Wizden mapping editor begin
+    public void SaveFavorites(List<MappingPrototype> prototypes)
+    {
+        var msg = new MappingFavoritesSaveMessage()
+        {
+            PrototypeIDs = prototypes
+                .FindAll(p => p.Prototype != null)
+                .Select(p => p.Prototype!.ID)
+                .ToList(),
+        };
+        _net.ClientSendMessage(msg);
+    }
+
+    public void LoadFavorites()
+    {
+        var request = new MappingFavoritesLoadMessage();
+        _net.ClientSendMessage(request);
+    }
+    //Reserve - Wizden mapping editor end
 }