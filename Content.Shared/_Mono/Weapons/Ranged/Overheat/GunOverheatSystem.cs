aed9a5f525b01547af73dd983c0cc5b52dfe04ca
NazrinNya <macrohard@macrohard.localhost>
2026-04-22 18:00:55 +0300

    rea

--- Content.Shared/_Mono/Weapons/Ranged/Overheat/GunOverheatSystem.cs
+++ Content.Shared/_Mono/Weapons/Ranged/Overheat/GunOverheatSystem.cs
@@ -19,6 +19,8 @@ public abstract class SharedGunOverheatSystem : EntitySystem
 
     public override void Update(float frameTime)
     {
+        sys
+
         if (_updateTimer < _updateCooldown)
         {
             _updateTimer += frameTime;
