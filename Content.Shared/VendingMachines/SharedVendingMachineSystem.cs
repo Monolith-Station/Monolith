1bfb8d04f173d7413b2b722ac1c6fe62ade6daa8
ThanosDeGraf <richardgirgindontstop@gmail.com>
2026-04-26 19:57:29 +0200

    moving more logic... looots of WiP still here.

--- Content.Shared/VendingMachines/SharedVendingMachineSystem.cs
+++ Content.Shared/VendingMachines/SharedVendingMachineSystem.cs
@@ -28,23 +28,17 @@ public abstract partial class SharedVendingMachineSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<CreditReceiverComponent, MapInitEvent>(OnMapInit); // Mono - Separation of cash from VendingMachineComp
+        SubscribeLocalEvent<VendingMachineComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<VendingMachineComponent, GotEmaggedEvent>(OnEmagged);
         SubscribeLocalEvent<VendingMachineComponent, GotUnEmaggedEvent>(OnUnemagged); // Frontier
 
         SubscribeLocalEvent<VendingMachineRestockComponent, AfterInteractEvent>(OnAfterInteract);
     }
 
-    /// <remarks> Mono - What sort of abysmal d*gsh*t is this, Frontier? </remarks>
-    protected virtual void OnMapInit(EntityUid uid, CreditReceiverComponent component, MapInitEvent args)
+    protected virtual void OnMapInit(EntityUid uid, VendingMachineComponent component, MapInitEvent args)
     {
         if (TryComp<VendingMachineComponent>(uid, out var vendingMachine)) // Mono
             RestockInventoryFromPrototype(uid, vendingMachine, vendingMachine.InitialStockQuality); // Mono
-
-        // Frontier: create the cash slot if this entity has one
-        if (component.CashSlot != null && component.CashSlotName != null)
-            ItemSlots.AddItemSlot(uid, component.CashSlotName, component.CashSlot);
-        // End Frontier
     }
 
     public void RestockInventoryFromPrototype(EntityUid uid,
--- Content.Shared/_Mono/Economy/SharedCreditReceiverSystem.cs
+++ Content.Shared/_Mono/Economy/SharedCreditReceiverSystem.cs
@@ -1,10 +1,16 @@
-﻿using Content.Shared._Mono.Economy.Component;
+﻿using System.Diagnostics.CodeAnalysis;
+using Content.Shared._Mono.Economy.Component;
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Stacks;
+using Content.Shared.VendingMachines;
 using Robust.Shared.Containers;
 
 namespace Content.Shared._Mono.Economy;
 
+/// <summary>
+/// Many systems and methods ripped out of <see cref="SharedVendingMachineSystem"/> and moved here. To handle machines that can accept any sort of currency and provide something in return.
+/// </summary>
+/// <remarks>Mostly to be used in other systems that like to implement this behavior to avoid code duplication.</remarks>
 public abstract partial class SharedCreditReceiverSystem : EntitySystem
 {
     [Dependency] private readonly SharedStackSystem _stack = default!; // Frontier
@@ -13,14 +19,20 @@ public abstract partial class SharedCreditReceiverSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<CreditReceiverComponent, EntInsertedIntoContainerMessage>(OnEntityInserted); // Frontier // Mono - Seperation of Cash from VendingMachineComp
-        SubscribeLocalEvent<CreditReceiverComponent, EntRemovedFromContainerMessage>(OnEntityRemoved); // Frontier // Mono - Seperation of Cash from VendingMachineComp
 
+        SubscribeLocalEvent<CreditReceiverComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<CreditReceiverComponent, EntInsertedIntoContainerMessage>(OnEntityInserted);
+        SubscribeLocalEvent<CreditReceiverComponent, EntRemovedFromContainerMessage>(OnEntityRemoved);
     }
 
-    // Frontier: cash slot logic
-    /// <remarks> Mono - This was seperated out from <see cref="VendingMachines"/> into its own system, since we'd like Ironman players to use shipyard consoles</remarks>>
-    private void OnEntityInserted(Entity<CreditReceiverComponent> ent, ref EntInsertedIntoContainerMessage args) // Mono - Seperation of Cash from VendingMachineComp
+    protected void OnMapInit(EntityUid uid, CreditReceiverComponent component, MapInitEvent args)
+    {
+        if (component.CashSlot != null && component.CashSlotName != null)
+            ItemSlots.AddItemSlot(uid, component.CashSlotName, component.CashSlot);
+    }
+
+
+    protected void Update(Entity<CreditReceiverComponent> ent)
     {
         if (ent.Comp.CashSlotName != null
             && ent.Comp.CurrencyStackType != null
@@ -37,9 +49,75 @@ public abstract partial class SharedCreditReceiverSystem : EntitySystem
         Dirty(ent, ent.Comp);
     }
 
-    private void OnEntityRemoved(Entity<CreditReceiverComponent> ent, ref EntRemovedFromContainerMessage args) // Mono
+    // Frontier: cash slot logic
+    /// <remarks> Mono - This was seperated out from <see cref="VendingMachines"/> into its own system, since we'd like Ironman players to use shipyard consoles</remarks>>
+    protected void OnEntityInserted(Entity<CreditReceiverComponent> ent, ref EntInsertedIntoContainerMessage args) // Mono - Seperation of Cash from VendingMachineComp
     {
-        ent.Comp.CashSlotBalance = 0;
-        Dirty(ent, ent.Comp);
+        Update(ent);
     }
+
+    protected void OnEntityRemoved(Entity<CreditReceiverComponent> ent, ref EntRemovedFromContainerMessage args) // Mono
+    {
+        Update(ent);
+    }
+
+    #region API
+
+    public bool CanPayWithCredit(Entity<CreditReceiverComponent> ent)
+    {
+        return TryComp<CreditReceiverComponent>(ent.Owner, out var creditComponent) && creditComponent.CashSlotName != null && creditComponent.CurrencyStackType != null;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="uid">EntityUID to be checked</param>
+    /// <param name="amount">If true, stores here the amount of currency found.</param>
+    /// <returns>Returns true if the entity has the CreditReceiverComponent and how much cash is stored. Returns false if the entity does not accept currency.</returns>
+    public bool TryGetCashBalance(EntityUid uid, [NotNullWhen(true)] out int? amount)
+    {
+        amount = null;
+
+        if (!TryComp<CreditReceiverComponent>(uid, out var receiver))
+        {
+            return false;
+        }
+
+        amount = receiver.CashSlotBalance;
+        return true;
+    }
+
+    public bool TryGetCashSlot(EntityUid uid, [NotNullWhen(true)] out ItemSlot? slot)
+    {
+        slot = null;
+
+        if (!TryComp<CreditReceiverComponent>(uid, out var receiver)) { return false; }
+
+        if (receiver == null || slot == null) { return false; } // Fixme - Possible null reference
+
+        slot = receiver.CashSlot!; // Fixme - Possible null reference
+        return true;
+    }
+
+    public bool TryGetCashEntity(EntityUid uid, [NotNullWhen(true)] out Entity<CreditReceiverComponent>? cashEntity)
+    {
+        cashEntity = null;
+        if (!TryComp<CreditReceiverComponent>(uid, out var receiver)) { return false; }
+
+        if (!TryGetCashSlot(uid, out var slot)
+            || slot == null)
+            return false;
+
+        if (!TryComp<StackComponent>(slot?.ContainerSlot?.ContainedEntity, out var stackComp)
+            && stackComp!.StackTypeId == receiver.CurrencyStackType)
+            return false;
+
+        if (slot!.ContainerSlot == null) // Fixme - possible null reference
+            return false;
+
+        cashEntity = slot.ContainerSlot!.ContainedEntity!.Value!;
+        return true;
+    }
+
+    #endregion
 }
