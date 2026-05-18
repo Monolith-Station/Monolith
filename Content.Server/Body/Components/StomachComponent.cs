d9d79be9986a4ca42ce12e0cbf721929cd6795de
10KE <151458027+10kee@users.noreply.github.com>
2026-05-18 12:03:39 +0100

    i will not use the yaml linter provided

--- Content.Server/Body/Components/StomachComponent.cs
+++ Content.Server/Body/Components/StomachComponent.cs
@@ -55,11 +55,6 @@ namespace Content.Server.Body.Components
         [DataField]
 
         /// <summary>
-        public bool SpecialExtraDiet = false;
-        ///     A bool for if the diet is exclusive to a whitelist. If they have additional foods they can eat on top of the human diet, that would be false. Else, it is true.
-        /// </summary>
-
-        /// <summary>
         ///     Used to track how long each reagent has been in the stomach
         /// </summary>
         [ViewVariables]
