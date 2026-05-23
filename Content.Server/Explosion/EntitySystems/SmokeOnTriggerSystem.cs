b6ae922d64e789f5b09eeaeb45303062efe3b8bc
Ilya246 <ilyukarno@gmail.com>
2026-05-24 00:49:29 +0400

    fix

--- Content.Server/Explosion/EntitySystems/SmokeOnTriggerSystem.cs
+++ Content.Server/Explosion/EntitySystems/SmokeOnTriggerSystem.cs
@@ -17,6 +17,7 @@ public sealed class SmokeOnTriggerSystem : SharedSmokeOnTriggerSystem
 {
     [Dependency] private readonly IMapManager _mapMan = default!;
     [Dependency] private readonly SmokeSystem _smoke = default!;
+    [Dependency] private readonly SharedMapSystem _map = default!;
     [Dependency] private readonly TransformSystem _transform = default!;
     [Dependency] private readonly SpreaderSystem _spreader = default!;
     [Dependency] private readonly TurfSystem _turf = default!;
@@ -32,17 +33,13 @@ public sealed class SmokeOnTriggerSystem : SharedSmokeOnTriggerSystem
     {
         var xform = Transform(uid);
         var mapCoords = _transform.GetMapCoordinates(uid, xform);
-        if (!_mapMan.TryFindGridAt(mapCoords, out _, out var grid) ||
-            !grid.TryGetTileRef(xform.Coordinates, out var tileRef) ||
-            tileRef.Tile.IsEmpty)
-        {
+        if (!_turf.TryGetTileRef(xform.Coordinates, out var tileRef) || tileRef.Tile.IsEmpty)
             return;
-        }
 
         if (_spreader.RequiresFloorToSpread(comp.SmokePrototype.ToString()) && _turf.IsSpace(tileRef))
             return;
 
-        var coords = grid.MapToGrid(mapCoords);
+        var coords = _map.MapToGrid(xform.GridUid!.Value, mapCoords);
         var ent = Spawn(comp.SmokePrototype, coords.SnapToGrid());
         if (!TryComp<SmokeComponent>(ent, out var smoke))
         {
