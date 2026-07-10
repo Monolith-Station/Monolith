2032475f3af32d2d8221710dc8336c700cb67af8
Eris <eris@erisws.com>
2026-07-09 20:52:07 -0400

    i plan to work on this later.

---
+++ Content.Shared/_CE/ZLevels/Gravity/GravityCountererSystem.cs
@@ -0,0 +1,18 @@
+/*
+ * This file is sublicensed under MIT License
+ * https://github.com/space-wizards/space-station-14/blob/master/LICENSE.TXT
+ */
+
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CE.ZLevels.Gravity;
+
+public abstract partial class GravityCountererSystem : EntitySystem
+{
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+}
