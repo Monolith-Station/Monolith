f5b3f1dc340846225cedbb68f89201384382d777
KAZNITOV81 <ivekh.10@gmail.com>
2026-07-13 13:15:16 +0900

    initial commit

--- Content.Client/Lobby/LobbyState.cs
+++ Content.Client/Lobby/LobbyState.cs
@@ -76,6 +76,7 @@ namespace Content.Client.Lobby
             _gameTicker.InfoBlobUpdated += UpdateLobbyUi;
             _gameTicker.LobbyStatusUpdated += LobbyStatusUpdated;
             _gameTicker.LobbyLateJoinStatusUpdated += LobbyLateJoinStatusUpdated;
+            _gameTicker.FactionCountUpdated += UpdateFactionCounts;
         }
 
         protected override void Shutdown()
@@ -85,6 +86,7 @@ namespace Content.Client.Lobby
             _gameTicker.InfoBlobUpdated -= UpdateLobbyUi;
             _gameTicker.LobbyStatusUpdated -= LobbyStatusUpdated;
             _gameTicker.LobbyLateJoinStatusUpdated -= LobbyLateJoinStatusUpdated;
+            _gameTicker.FactionCountUpdated -= UpdateFactionCounts;
             _contentAudioSystem.LobbySoundtrackChanged -= UpdateLobbySoundtrackInfo;
 
             _voteManager.ClearPopupContainer();
@@ -244,6 +246,25 @@ namespace Content.Client.Lobby
                 Lobby!.Background.Texture = null;
             }
 
+        }
+
+        private void UpdateFactionCounts()
+        {
+            if (!_gameTicker.IsGameStarted)
+            {
+                Lobby!.FactionCountLabel.SetMarkup(Loc.GetString(
+                    "lobby-faction-ready-count",
+                    ("totalReady", _gameTicker.TotalReady),
+                    ("bluforReady", _gameTicker.BluforReady),
+                    ("redforReady", _gameTicker.RedforReady)));
+            }
+            else
+            {
+                Lobby!.FactionCountLabel.SetMarkup(Loc.GetString(
+                    "lobby-faction-alive-count",
+                    ("bluforAlive", _gameTicker.BluforAlive),
+                    ("redforAlive", _gameTicker.RedforAlive)));
+            }
         }
 
         private void SetReady(bool newReady)
