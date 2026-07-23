2e9b74b8fad5ec6304c627916f4ed3e24595ee68
naz <naz@foid.itshim.org>
2026-07-23 23:11:03 +0300

    mprovements

--- Content.Server/_Mono/Radiation/ChainRadiationSystem.cs
+++ Content.Server/_Mono/Radiation/ChainRadiationSystem.cs
@@ -8,6 +8,7 @@ using Content.Shared.Radiation.Components;
 namespace Content.Server._Mono.Radiation;
 
 public sealed partial class ChainRadiationSystem : EntitySystem
+
 {
     [Dependency] private EntityLookupSystem _lookup = default!;
     [Dependency] private ExplosionSystem _explosion = default!;
