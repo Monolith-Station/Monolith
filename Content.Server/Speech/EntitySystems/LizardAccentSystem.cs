22eaf24c0b53a9edc39c01c0d559ebe390b29bf5
teasum <158848664+teasum@users.noreply.github.com>
2026-08-11 17:26:32 +0300

    Перевод эмоций и акцент унатхов (#359)
    * добавил перевод эмоутам и локализацию chattriggers
    
    * локализировал акцент унатхам
    
    * убрал мусор
    
    * фиксы
    
    * добавил акцент еа щ унатхам
    
    * RegexUpperSch теперь существует

--- Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
@@ -1,6 +1,5 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
-
 namespace Content.Server.Speech.EntitySystems;
 
 public sealed class LizardAccentSystem : EntitySystem
@@ -10,7 +9,18 @@ public sealed class LizardAccentSystem : EntitySystem
     private static readonly Regex RegexInternalX = new(@"(\w)x");
     private static readonly Regex RegexLowerEndX = new(@"\bx([\-|r|R]|\b)");
     private static readonly Regex RegexUpperEndX = new(@"\bX([\-|r|R]|\b)");
-
+    //Forge-Localization-Start
+    private static readonly Regex RegexLowerC = new Regex("с+");
+    private static readonly Regex RegexUpperC = new Regex("С+");
+    private static readonly Regex RegexLowerZ = new Regex("з+");
+    private static readonly Regex RegexUpperZ = new Regex("З+");
+    private static readonly Regex RegexLowerSh = new Regex("ш+");
+    private static readonly Regex RegexUpperSh = new Regex("Ш+");
+    private static readonly Regex RegexLowerCh = new Regex("ч+");
+    private static readonly Regex RegexUpperCh = new Regex("Ч+");
+    private static readonly Regex RegexLowerSch = new Regex("щ+");
+    private static readonly Regex RegexUpperSch = new Regex("Щ+");
+    //Forge-Localiztion-End
     public override void Initialize()
     {
         base.Initialize();
@@ -31,7 +41,18 @@ public sealed class LizardAccentSystem : EntitySystem
         message = RegexLowerEndX.Replace(message, "ecks$1");
         // eckS
         message = RegexUpperEndX.Replace(message, "ECKS$1");
-
+        //Forge-Localization-Start
+        message = RegexLowerC.Replace(message, "ссс");
+        message = RegexUpperC.Replace(message, "ССС");
+        message = RegexLowerZ.Replace(message, "ссс");       // для "з+" используются те же замены, что и для "с+"
+        message = RegexUpperZ.Replace(message, "ССС"); // для "З+" используются те же замены, что и для "С+"
+        message = RegexLowerSh.Replace(message, "шшш");
+        message = RegexUpperSh.Replace(message, "ШШШ");
+        message = RegexLowerCh.Replace(message, "ччч");
+        message = RegexUpperCh.Replace(message, "ЧЧЧ");
+        message = RegexLowerSch.Replace(message, "щщщ");
+        message = RegexUpperSch.Replace(message, "ЩЩЩ");
+        //Forge-Localization-End
         args.Message = message;
     }
 }
