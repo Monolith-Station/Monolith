c9f93766ff46031524c4958d8d81e15205fbdcd0
Super <84590915+SuperGDPWYL@users.noreply.github.com>
2026-09-12 19:34:02 +0100

    The Great Heist (Solarian Vault Upgrade: Solarian DC Storage Server) (#4682)

--- Content.Server/_NF/StationEvents/Events/BluespaceErrorRule.cs
+++ Content.Server/_NF/StationEvents/Events/BluespaceErrorRule.cs
@@ -23,6 +23,7 @@ using Content.Server._Mono.StationEvents;
 using Content.Server._Mono.GridClaimer;
 using Content.Server._Mono.Store.Components;
 using Content.Server._Mono.Store;
+using Newtonsoft.Json;
 
 namespace Content.Server._NF.StationEvents.Events;
 
@@ -265,6 +266,30 @@ public sealed partial class BluespaceErrorRule : StationEventSystem<BluespaceErr
                 }
 
                 var gridValue = _pricing.AppraiseGrid(gridUid, null);
+                // Mono: currency injections
+                if (TryComp<CurrencyInjectionOnBluespaceErrorComponent>(uid, out var comp))
+                {
+                    var childQuery = Transform(gridUid).ChildEnumerator;
+                    var requiredEntitiesFound = true;
+                    foreach (var entProtoId in comp.RequiredEntities)
+                    {
+                        requiredEntitiesFound = false;
+                        while (childQuery.MoveNext(out var entity))
+                        {
+                            var metaData = MetaData(entity);
+                            if (metaData.EntityPrototype != null && entProtoId.Equals(metaData.EntityPrototype.ID))
+                            {
+                                requiredEntitiesFound = true;
+                                break;
+                            }
+                        }
+                        if (!requiredEntitiesFound)
+                            break;
+                    }
+                    if (gridValue / component.StartingValue > comp.IntegrityRequirement && requiredEntitiesFound) // good job!
+                        _currencyInjection.InjectCurrency(comp.Company, comp.Amount);
+                }
+                // Mono end
 
                 // Deletion has to happen before grid traversal re-parents players.
                 Del(gridUid);
@@ -279,14 +304,6 @@ public sealed partial class BluespaceErrorRule : StationEventSystem<BluespaceErr
                     var reward = (int)(gridValue * rewardCoeff);
                     _bank.TrySectorDeposit(account, reward, LedgerEntryType.BluespaceReward);
                 }
-
-                // Mono: currency injections
-                if (TryComp<CurrencyInjectionOnBluespaceErrorComponent>(uid, out var comp))
-                {
-                    if (gridValue / component.StartingValue > comp.IntegrityRequirement) // good job!
-                        _currencyInjection.InjectCurrency(comp.Company, comp.Amount);
-                }
-                // Mono end
             }
         }
 
