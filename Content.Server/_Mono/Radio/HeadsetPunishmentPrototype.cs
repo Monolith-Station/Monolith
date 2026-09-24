b27600a62726bb9e8e279dd0346a764b2c6226b2
Eris <eris@erisws.com>
2026-09-24 10:25:19 -0400

    anti-slur system

---
+++ Content.Server/_Mono/Radio/HeadsetPunishmentPrototype.cs
@@ -0,0 +1,29 @@
+using Content.Shared.Explosion;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._Mono.Radio;
+
+[Prototype]
+public sealed partial class HeadsetPunishmentPrototype : IPrototype
+{
+    [IdDataField]
+    public string ID { get; private set; } = default!;
+
+    [DataField(required: true)]
+    public List<string> Words = new();
+
+    [DataField]
+    public ProtoId<ExplosionPrototype> Explosion = "HeadsetPunishment";
+
+    [DataField]
+    public float BaseIntensity = 8f;
+
+    [DataField]
+    public float IntensityPerMatch = 8f;
+
+    [DataField]
+    public float MaxIntensity = 40f;
+
+    [DataField]
+    public float StunSecondsPerMatch = 2f;
+}
